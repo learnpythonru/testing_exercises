9df0f474ae91fdd3552fdca68e76fed2d0999896
DianaRatnikova <Diana_r@bk.ru>
2023-05-13 18:20:49 +0300

    Added test_one_gender_type

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -11,4 +11,11 @@ def test_genderalize():
     assert not genderalize(verb_male, verb_female, gender_else) == verb_male
     assert not genderalize(verb_male, verb_female, gender_male) == verb_female
     assert genderalize(verb_male, verb_female, '254654') == verb_female
-    
+
+
+def test_genderalize_type():
+    verb_male = "VM"
+    verb_female="VF"
+    gender_male = "male"
+    gender_else = "female"
+    assert type(genderalize(verb_male, verb_female, gender_male)) == str
