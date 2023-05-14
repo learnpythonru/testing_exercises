2c543279f92ea59b8f74dbf03cc1e72715ae70fb
DianaRatnikova <Diana_r@bk.ru>
2023-05-14 12:47:06 +0300

    Added test_three

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,4 +2,6 @@ from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    pass
+    get_params = {'k': 'k_str', 'v': 'v_str'}
+    assert build_url('host_name', 'relative_url') ==  'host_name/relative_url'
+    assert build_url('str1', 'relative_url', get_params) == 'str1/relative_url?k=k_str&v=v_str'
