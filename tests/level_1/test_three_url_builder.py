79561574976494ba750f678c00bf4a907fe1151e
DianaRatnikova <Diana_r@bk.ru>
2023-05-17 01:02:32 +0300

    Edited program3 and 4

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -1,7 +1,15 @@
 from functions.level_1.three_url_builder import build_url
-
+import pytest
 
 def test_build_url():
     get_params = {'k': 'k_str', 'v': 'v_str'}
     assert build_url('host_name', 'relative_url') ==  'host_name/relative_url'
     assert build_url('str1', 'relative_url', get_params) == 'str1/relative_url?k=k_str&v=v_str'
+
+
+def test_three_value_error():
+    with pytest.raises(ValueError, match="ValueError"):
+        raise ValueError("ValueError!!!")
+        build_url(["202yhbmj5", 1], 14645)
+        build_url({'fere': 0}, 134, "Python", 1,2,3 )
+        build_url(1, {"32":21, '323':1}, 0)
