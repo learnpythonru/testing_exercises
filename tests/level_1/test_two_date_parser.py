85bec9b2b822398c6af27b9abaa813f58b3cb4ee
DianaRatnikova <Diana_r@bk.ru>
2023-05-16 23:31:10 +0300

    Edited program2

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -5,4 +5,4 @@ def test_compose_datetime_from():
     assert compose_datetime_from("2023,12,15", "19:55") == datetime.datetime(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day,19,55)
     assert compose_datetime_from("tomorrow", "19:55") == datetime.datetime(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day+1,19,55)
     assert compose_datetime_from("202yhbmj5", "19:33") == datetime.datetime(datetime.date.today().year,datetime.date.today().month,datetime.date.today().day,19,33)
-    pass
+
