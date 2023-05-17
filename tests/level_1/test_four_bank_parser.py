f4d280f0c96504e63772c7150ac580b19ff47dd1
Konstantin Mishakov <mishakov1c@gmail.com>
2023-05-17 22:23:29 +0400

    done

--- tests/level_1/test_four_bank_parser.py
+++ tests/level_1/test_four_bank_parser.py
@@ -1,5 +1,9 @@
 from functions.level_1.four_bank_parser import BankCard, SmsMessage, Expense, parse_ineco_expense
-
+from datetime import datetime
 
 def test_parse_ineco_expense():
-    pass
+    message = SmsMessage('100 руб, *1234 12.04.23 16:00 nenaprasno.ru authcode 0000', "Tinkoff", datetime.now())
+    cards = [BankCard('1234', 'KONSTANTIN MISHAKOV'), BankCard('5678', 'KONSTANTIN MISHAKOV')]
+
+    assert parse_ineco_expense(message, cards)
+     
