e9904222ff7b44f4780bca5521253e570bd1de8e
Konstantin Mishakov <mishakov1c@gmail.com>
2023-05-18 19:50:27 +0400

    done

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -2,4 +2,8 @@ from functions.level_1.five_title import change_copy_item
 
 
 def test_change_copy_item():
-    pass
+    title = 'Hello World!'
+    
+    assert change_copy_item(title, 20) == title
+    assert change_copy_item(title, 100) == 'Copy of Hello World!'
+    assert change_copy_item('Copy of Hello World! (3)', 100) == 'Copy of Hello World! (4)'
