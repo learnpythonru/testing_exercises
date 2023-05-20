61d355cf4a93e882f078d14c4f4412a38ff1b7c8
Konstantin Mishakov <mishakov1c@gmail.com>
2023-05-20 19:46:47 +0400

    small fix

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -6,7 +6,7 @@ def test__compose_datetime_from__today():
     today = datetime.date.today()
     hour_str = '17'
     minute_str = '15'
-    expected_today = datetime(today.year, today.month, today.day, int(hour_str), int(minute_str), )
+    expected_today = datetime.datetime(today.year, today.month, today.day, int(hour_str), int(minute_str), )
     
     result = compose_datetime_from('today', "17:15")
 
@@ -18,7 +18,7 @@ def test__compose_datetime_from__tomorrow():
     hour_str = '17'
     minute_str = '15'
     tomorrow = today + datetime.timedelta(1) 
-    expected_tomorrow = datetime(tomorrow.year, tomorrow.month, tomorrow.day, int(hour_str), int(minute_str), )
+    expected_tomorrow = datetime.datetime(tomorrow.year, tomorrow.month, tomorrow.day, int(hour_str), int(minute_str), )
 
     result = compose_datetime_from('tomorrow', "17:15") 
 
