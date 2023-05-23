8a7a81f7097356f1f4d09aff73286202967dbc00
DianaRatnikova <Diana_r@bk.ru>
2023-05-23 12:45:07 +0300

    Added tests for tasks 2-5

--- tests/level_1_5/test_one_median.py
+++ tests/level_1_5/test_one_median.py
@@ -1,52 +1,52 @@
 from functions.level_1_5.one_median import get_median_value
 import pytest
 
-def test__get_median_value_empty_params():
+def test__get_median_value__empty_params():
     items = ''
     get_median_value_result = get_median_value(items)
     assert get_median_value_result == None
 
 
-def test__get_median_value_list_of_three_params():
+def test__get_median_value__list_of_three_params():
     items = [2,1,5]
     get_median_value_result = get_median_value(items)
     assert get_median_value_result == 5
 
 
-def test__get_median_value_list_of_negative_values():
+def test__get_median__value__list_of_negative_values():
     items = [-2,-1,5]
     get_median_value_result = get_median_value(items)
     assert get_median_value_result == 5
 
 
-def test__get_median_value_list_of_odd_params():
+def test__get_median__value__list_of_odd_params():
     items = [2,1,3,9, 10]
     get_median_value_result = get_median_value(items)
     assert get_median_value_result == 9
 
 
-def test__get_median_value_list_of_even_params():
+def test__get_median__value__list_of_even_params():
     items = [2,1,1,1,3,3,2,9,9,3,9, 10]
     get_median_value_result = get_median_value(items)
     assert get_median_value_result == 9
 
 
-def test__get_median_value_list_of_float_params():
+def test__get_median__value__list_of_float_params():
     items = [0.6, 99.4, 13.2]
     get_median_value_result = get_median_value(items)
     assert get_median_value([0.6, 99.4, 13.2])==13.2
 
-# Вопрос: а как наложить принцип ААА на проверкутаких ошибок?
-def test__get_median_value_no_params_typeerror():
+
+def test__get_median_value__no_params_typeerror():
         with pytest.raises(TypeError):
             get_median_value()
 
 
-def test__get_median_list_of_four_params_indexerror():
+def test__get_median_value__list_of_four_params_indexerror():
         with pytest.raises(IndexError):
             get_median_value([2,1,3,9])
 
 
-def test__get_median_one_int_in_params_typeerror():
+def test__get_median_value__one_int_in_params_typeerror():
         with pytest.raises(TypeError):
             get_median_value(1)
---
+++ tests/level_1_5/test_two__square_equation.py
@@ -0,0 +1,53 @@
+from functions.level_1_5.two_square_equation import solve_square_equation
+import pytest
+
+
+def test__solve_square_equation__all_params_zero():
+    assert solve_square_equation(0,0,0) == (None, None)
+
+
+def test__solve_square_equation__const_coefficient_zero(): 
+    assert solve_square_equation(5,2,0) == (-0.4, 0.0)
+
+
+def test__solve_square_equation__square_coefficient_zero():     
+    assert solve_square_equation(0,2,10) == (-5.0, None)
+
+
+def test__solve_square_equation__only_linear_coefficient():
+    assert solve_square_equation(0,2,0) == (0.0, None)
+
+
+def test__solve_square_equation__linear_coefficient_zero():
+    assert solve_square_equation(4,0,-16) == (-2.0, 2.0)
+
+
+def test__solve_square_equation__all_coefficients_int():
+    assert solve_square_equation(5,3,-26) == (-2.6, 2.0)
+
+
+def test__solve_square_equation__discriminant_below_zero():
+    assert solve_square_equation(1,2,5) == (None, None)
+
+
+def test__solve_square_equation__all_coefficients_float():
+    assert solve_square_equation(1.1, 2.2, -5.5) == (-3.449489742783178, 1.4494897427831779)
+
+
+def test__solve_square_equation__not_enougt_params():
+    with pytest.raises(TypeError):
+        solve_square_equation(0,0)
+
+
+def test__solve_square_equation__too_many_params():
+    with pytest.raises(TypeError):
+        solve_square_equation(5,2,0, 9)
+
+
+def test__solve_square_equation__params_are_string():
+    with pytest.raises(TypeError):
+        solve_square_equation('a','b','c')
+
+def test__solve_square_equation__params_are_lists():
+    with pytest.raises(TypeError):
+        solve_square_equation([0],[0],[0])
