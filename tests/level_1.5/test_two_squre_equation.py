a0a24fa102277898db19ad9115afe1dc43db9f08
Dmitry Efimov <edwa14011977@gmail.com>
2023-05-30 21:11:02 +0300

    Change some tests to parametrize

--- tests/level_1.5/test_two_squre_equation.py
+++ tests/level_1.5/test_two_squre_equation.py
@@ -10,14 +10,14 @@ def test__solve_square_equation__discriminant_below_zero():
     assert solve_square_equation(1, 1, 1) == (None, None)
 
 
-@pytest.mark.xfail(reason='should be only one root for the equation when dicriminant is zero')
-def test__solve_square_equation__discriminant_is_zero():
-    assert solve_square_equation(1, 2, 1) == (-1.0, None)
-
-
 def test__solve_square_equation__a_is_zero_b_is_zero():
     assert solve_square_equation(0, 0, 1) == (None, None)
 
 
 def test__solve_square_equation__a_is_zero():
-    assert solve_square_equation(0, -2, 2) == (1, None)
+    assert solve_square_equation(0, -2, 2) == (1.0, None)
+
+
+@pytest.mark.xfail(reason='should be only one root for the equation when dicriminant is zero')
+def test__solve_square_equation__discriminant_is_zero():
+    assert solve_square_equation(1, 2, 1) == (-1.0, None)
--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,6 +1,7 @@
 import datetime
 from freezegun import freeze_time
 from functions.level_1.two_date_parser import compose_datetime_from
+import pytest
 
 
 @freeze_time("2023-05-16")
@@ -14,5 +15,5 @@ def test__compose_datetime_from__tomorrow():
 
 
 @freeze_time("2023-05-16")
-def test_compose_datetime_from_yeaterday():
+def test_compose_datetime_from_yeasterday():
     assert compose_datetime_from("yesterday", "12:00") == datetime.datetime(2023, 5, 16, 12, 0)
