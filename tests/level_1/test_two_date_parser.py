688b509d86ecd9e4e047a2a83980542034479453
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-02 17:47:29 +0300

    Test two: compose_datetime_from

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,24 @@
+import datetime
+
 from functions.level_1.two_date_parser import compose_datetime_from
 
 
 def test_compose_datetime_from():
-    pass
+    # Динамические значения
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+    # Проверки
+    assert compose_datetime_from('tomorrow', '09:11') == datetime.datetime(
+        tomorrow.year,
+        tomorrow.month,
+        tomorrow.day,
+        9,
+        11
+    )
+    assert compose_datetime_from('not tomorrow', '09:11') == datetime.datetime(
+        today.year,
+        today.month,
+        today.day,
+        9,
+        11
+    )
