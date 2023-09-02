57b2063660ce269dfe52ae243c0d16888d498247
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-02 17:51:40 +0300

    Test five: change_copy_item

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -2,4 +2,8 @@ from functions.level_1.five_title import change_copy_item
 
 
 def test_change_copy_item():
-    pass
+    # Проверки
+    assert change_copy_item('Item') == 'Copy of Item'
+    assert change_copy_item('Item', 4) == 'Item'
+    assert change_copy_item('Copy of Item') == 'Copy of Item (2)'
+    assert change_copy_item('Copy of Item (2)') == 'Copy of Item (3)'
