e118c8ba380e7ae3347213065647e81bbf38eef7
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-02 17:46:33 +0300

    Test one: genderalize

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -2,4 +2,13 @@ from functions.level_1.one_gender import genderalize
 
 
 def test_genderalize():
-    pass
+    # Проверки
+    assert genderalize(
+        verb_male='сделал', verb_female='сделала', gender='male'
+    ) == 'сделал'
+    assert genderalize(
+        verb_male='сделал', verb_female='сделала', gender='female'
+    ) == 'сделала'
+    assert genderalize(
+        verb_male='сделал', verb_female='сделала', gender='genderfluid'
+    ) == 'сделала'
