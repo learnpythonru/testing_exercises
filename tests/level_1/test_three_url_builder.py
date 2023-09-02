797be4115eb6d7f7081e1fb4dbac8975f052732a
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-02 17:49:23 +0300

    Test three: build_url

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,4 +2,13 @@ from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    pass
+    # Проверки
+    assert build_url(
+        host_name='https://github.com',
+        relative_url='HarrierHarrier/testing_exercises'
+    ) == 'https://github.com/HarrierHarrier/testing_exercises'
+    assert build_url(
+        host_name='https://github.com',
+        relative_url='HarrierHarrier/testing_exercises',
+        get_params={'a': 1, 'b': '2'}
+    ) == 'https://github.com/HarrierHarrier/testing_exercises?a=1&b=2'
