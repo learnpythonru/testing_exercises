e9e78e865b2ae715904549ff1c642340fe2e9145
Mischa <mishayartsev@gmail.com>
2023-09-05 18:27:11 +0300

    +thrid test

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,4 +2,4 @@ from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    pass
+    assert build_url("https://confluence.hz.ru/", "pages/viewpage.action", {'pageId': '7569538209'}) == "https://confluence.hz.ru//pages/viewpage.action?pageId=7569538209"
