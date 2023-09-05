e75af7ed6e31c7c5fd5dc38804b112a7e21cc815
Mischa <mishayartsev@gmail.com>
2023-09-05 18:02:48 +0300

    +second test

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,22 @@
+import datetime
+
 from functions.level_1.two_date_parser import compose_datetime_from
 
 
+def check_datetime(date_str: str, time_str: str) -> datetime.datetime:
+    # Had to write same function to get same datetime everyday
+    date = datetime.date.today()
+    if date_str == "tomorrow":
+        date += datetime.timedelta(days=1)
+    
+    hour_str, minute_str = time_str.strip().split(":")
+    return datetime.datetime(
+        date.year,
+        date.month,
+        date.day,
+        int(hour_str),
+        int(minute_str),
+    )
+
 def test_compose_datetime_from():
-    pass
+    assert compose_datetime_from('tomorrow', '13:37') == check_datetime('tomorrow', '13:37')
