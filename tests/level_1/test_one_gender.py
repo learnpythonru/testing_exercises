59b7ab2efc6bef97842bfb7a4ec0ee81d894905d
Mischa <mishayartsev@gmail.com>
2023-09-05 16:40:50 +0300

    +first test

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -2,4 +2,5 @@ from functions.level_1.one_gender import genderalize
 
 
 def test_genderalize():
-    pass
+    # verb_male if gender == "male" else verb_female
+    assert genderalize('otec', 'mat', 'male') == 'otec'
