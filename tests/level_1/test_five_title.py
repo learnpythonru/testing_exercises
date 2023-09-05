a886042ae35d58943df0fa3cea6b1bc816f3d942
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-05 17:20:23 +0300

    fifth ex

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -1,5 +1,38 @@
 from functions.level_1.five_title import change_copy_item
+import pytest
 
 
-def test_change_copy_item():
-    pass
+@pytest.mark.parametrize(
+    'title,expected',
+    [
+        ("Hamlet", 'Copy of Hamlet'),
+        ("Hamlet (105)", 'Copy of Hamlet (105)'),
+        ("Copy of Hamlet (105)", 'Copy of Hamlet (106)'),
+        ("Copy of Hamlet (-10)", 'Copy of Hamlet (-10) (2)'),
+        ("Copy of Hamlet (by William Shakespeare)", 'Copy of Hamlet (by William Shakespeare) (2)'),
+        ("", 'Copy of '),
+    ]
+)
+def test_change_copy_item(title, expected):
+    assert change_copy_item(title) == expected
+
+
+@pytest.mark.parametrize(
+    'title,main_item_title_length,expected',
+    [
+        ("Hamlet", 5, 'Hamlet'),
+        ("Hamlet (105)", 5, 'Hamlet (105)'),
+        ("Hamlet", 15, 'Copy of Hamlet')
+    ]
+)
+def test_change_copy_item_with_max_main_item_title_length(title, main_item_title_length, expected):
+    assert change_copy_item(title, main_item_title_length) == expected
+
+
+def test_change_copy_item_with_wrong_max_main_item_title_length():
+    with pytest.raises(TypeError):
+        change_copy_item("Hamlet", 'two')
+
+def test_change_copy_item_with_wrong_title():
+    with pytest.raises(AttributeError):
+        change_copy_item(10)
