d7b29ca5414373c775b1a1446ab3c51bbd8adf28
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-05 09:23:09 +0300

    first ex

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -1,5 +1,14 @@
 from functions.level_1.one_gender import genderalize
+import pytest
 
 
-def test_genderalize():
-    pass
+@pytest.mark.parametrize(
+    "verb_male,verb_female,gender,expected",
+    [
+        ('сделал', 'сделала', 'male', 'сделал'),
+        ('сделал', 'сделала', 'female', 'сделала'),
+        (False, 'сделала', 'male', False),
+        (0, 1, 10, 1),
+    ])
+def test_genderalize(verb_male, verb_female, gender, expected):
+    assert genderalize(verb_male, verb_female, gender) == expected
