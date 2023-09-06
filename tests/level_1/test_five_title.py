aa43cd060b6df00f9974e7dfd39d6e2047bdb57f
Mischa <mishayartsev@gmail.com>
2023-09-06 16:15:39 +0300

    +fifth test

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -2,4 +2,7 @@ from functions.level_1.five_title import change_copy_item
 
 
 def test_change_copy_item():
-    pass
+    parameters = 'Copy of '
+    test_title = 'There is something in the air tonight, my dear. I swear, I saw it with my own eyes.'
+    
+    assert change_copy_item(title=test_title) == parameters + test_title
