c828fbcccec9f19ee0ef9e2081a684bd919a6e4f
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-13 22:09:58 +0300

    Test one: separate test cases

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -1,14 +1,19 @@
 from functions.level_1.one_gender import genderalize
 
 
-def test_genderalize():
-    # Проверки
+def test__genderalize__check_male_verb():
     assert genderalize(
         verb_male='сделал', verb_female='сделала', gender='male'
     ) == 'сделал'
+
+
+def test__genderalize__check_female_verb():
     assert genderalize(
         verb_male='сделал', verb_female='сделала', gender='female'
     ) == 'сделала'
+
+
+def test__genderalize__check_another_verb():
     assert genderalize(
         verb_male='сделал', verb_female='сделала', gender='genderfluid'
     ) == 'сделала'
