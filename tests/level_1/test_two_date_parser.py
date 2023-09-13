986b1c1cc8499467c82a36e017c4a8d2b7d1cc12
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-13 22:10:57 +0300

    Test two: separate test cases, freezegun & exceptions testing

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,24 +1,34 @@
 import datetime
 
+from freezegun import freeze_time
+import pytest
+
 from functions.level_1.two_date_parser import compose_datetime_from
 
 
-def test_compose_datetime_from():
-    # Динамические значения
-    today = datetime.date.today()
-    tomorrow = today + datetime.timedelta(days=1)
-    # Проверки
-    assert compose_datetime_from('tomorrow', '09:11') == datetime.datetime(
-        tomorrow.year,
-        tomorrow.month,
-        tomorrow.day,
-        9,
-        11
-    )
-    assert compose_datetime_from('not tomorrow', '09:11') == datetime.datetime(
-        today.year,
-        today.month,
-        today.day,
-        9,
-        11
-    )
+@freeze_time('2023-01-01')
+def test__compose_datetime_from__date_str_tomorow():
+    test_value = datetime.datetime.now() + datetime.timedelta(days=1)
+
+    value = compose_datetime_from('tomorrow', '00:00')
+
+    assert value == test_value
+
+
+@freeze_time('2023-01-01')
+def test__compose_datetime_from__date_str_not_tomorow():
+    test_value = datetime.datetime.now()
+
+    value = compose_datetime_from('not tomorrow', '00:00')
+
+    assert value == test_value
+
+
+def test__compose_datetime_from__invalid_time_str_value():
+    with pytest.raises(ValueError):
+        compose_datetime_from('not tomorrow', '0911')
+
+
+def test__compose_datetime_from__invalid_time_str_type():
+    with pytest.raises(AttributeError):
+        compose_datetime_from('not tomorrow', None)
