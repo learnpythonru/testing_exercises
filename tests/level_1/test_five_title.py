8b1936a7d1679f3557ba830573f6a9548d89b041
Vitaly Lunev <v.lunev.mail@gmail.com>
2023-09-13 22:12:05 +0300

    Test five: separate test cases

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -1,9 +1,17 @@
 from functions.level_1.five_title import change_copy_item
 
 
-def test_change_copy_item():
-    # Проверки
+def test__change_copy_item__add_prefix():
     assert change_copy_item('Item') == 'Copy of Item'
-    assert change_copy_item('Item', 4) == 'Item'
+
+
+def test__change_copy_item__add_copy_number():
     assert change_copy_item('Copy of Item') == 'Copy of Item (2)'
+
+
+def test__change_copy_item__increase_copy_number():
     assert change_copy_item('Copy of Item (2)') == 'Copy of Item (3)'
+
+
+def test__change_copy_item__check_max_length():
+    assert change_copy_item('Item', 4) == 'Item'
