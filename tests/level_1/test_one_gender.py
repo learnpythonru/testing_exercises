d984c50c58c17d4e856e3cd0868e626d197df430
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-18 19:04:55 +0300

    one_gender complite

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -1,5 +1,13 @@
 from functions.level_1.one_gender import genderalize
+import pytest
 
 
-def test_genderalize():
-    pass
+@pytest.mark.parametrize('verb_male, verb_female, gender, expected_result', [
+    ('Красивый', 'Красивая', 'male', 'Красивый'),
+    ('Спортивный', 'Спортивная', 'female', 'Спортивная'),
+    ('Активный', 'Активная', 'unknown', 'Активная'),
+])
+def test_genderalize(verb_male: str, verb_female: str, gender: str,
+                     expected_result: str):
+    assert genderalize(verb_male=verb_male, verb_female=verb_female,
+                       gender=gender) == expected_result
