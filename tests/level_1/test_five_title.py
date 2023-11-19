4130f678fae8eaf1529e7afe22a2186eb606fcad
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-19 18:01:50 +0300

    add tests for five_title

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -1,5 +1,33 @@
+import pytest
+
 from functions.level_1.five_title import change_copy_item
 
 
-def test_change_copy_item():
-    pass
+@pytest.mark.parametrize('title, max_title_lenght, expected_result', [
+    ('Some title', 100, 'Copy of Some title'),
+    ('Copy of Some title', 100, 'Copy of Some title (2)'),
+    ('Copy of Some title (5)', 10, 'Copy of Some title (5)'),
+])
+def test_change_copy_item(title, max_title_lenght, expected_result):
+    assert change_copy_item(title=title,
+                            max_main_item_title_length=max_title_lenght
+                            ) == expected_result
+
+
+big_title = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " \
+             "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " \
+             "when an unknown printer took a galley of type and scrambled it to make a type " \
+             "specimen book. It has survived not only five centuries, but also the leap into " \
+             "electronic typesetting, remaining essentially unchanged. It was popularised in " \
+             "the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, " \
+             "and more recently with desktop publishing software like Aldus PageMaker including " \
+             "versions of Lorem Ipsum."
+
+
+@pytest.mark.parametrize('title, expected_result', [
+    ('Some title', 'Copy of Some title'),
+    ('Copy of Some title', 'Copy of Some title (2)'),
+    (big_title, big_title),
+])
+def test_change_copy_item_with_default_max_lenght(title, expected_result):
+    assert change_copy_item(title=title) == expected_result
