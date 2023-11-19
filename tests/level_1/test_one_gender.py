6378ce040cfa2b960ba338c2e3a69780368b0a67
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-19 15:17:52 +0300

    tests for two_date_parser

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -1,5 +1,6 @@
-from functions.level_1.one_gender import genderalize
 import pytest
+
+from functions.level_1.one_gender import genderalize
 
 
 @pytest.mark.parametrize('verb_male, verb_female, gender, expected_result', [
--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,31 @@
+from datetime import datetime, timedelta
+
+import pytest
+
 from functions.level_1.two_date_parser import compose_datetime_from
 
 
-def test_compose_datetime_from():
-    pass
+@pytest.mark.parametrize('date_str, time_str, expected_result', [
+    ('tomorrow', '00:00', (datetime(datetime.now().year, datetime.now().month,
+                           datetime.now().day) + timedelta(days=1)).replace(
+                               hour=00, minute=00, second=00,)),
+    ('today', '12:25', (datetime(datetime.now().year, datetime.now().month,
+                                 datetime.now().day).replace(
+                                     hour=12, minute=25, second=00))),
+    ('2023-11-18', '13:45', (datetime(datetime.now().year,
+                                      datetime.now().month,
+                                      datetime.now().day).replace(
+                                     hour=13, minute=45, second=00))),
+])
+def test_compose_datetime_from(date_str, time_str, expected_result):
+    assert compose_datetime_from(date_str=date_str,
+                                 time_str=time_str) == expected_result
+
+
+@pytest.mark.parametrize('date_str, time_str', [
+    ('2023-11-18', '13:45:00'),
+    ('yesterday', ''),
+])
+def test_compose_datetime_from_with_error(date_str, time_str):
+    with pytest.raises(ValueError):
+        compose_datetime_from(date_str=date_str, time_str=time_str)
