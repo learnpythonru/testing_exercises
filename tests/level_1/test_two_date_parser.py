a8eff06feb82f331d5dff973ced5ee49472cf1e6
fox <fox@kond.me>
2023-11-23 22:19:39 +0300

    Test two date half finished

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,8 @@
-from functions.level_1.two_date_parser import compose_datetime_from
+import datetime
+from freezegun import freeze_time
 
-
+@freeze_time("2012-01-01")
 def test_compose_datetime_from():
-    pass
+    from functions.level_1.two_date_parser import compose_datetime_from
+
+    assert compose_datetime_from("",time_str="18:12") == datetime.datetime(2012,1,1,18,12)
