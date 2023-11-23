7d1448b62ab2d2cd9e4ffc8be6199299ac336568
Denis Kondrashov <dkond@mail.ru>
2023-11-23 23:13:19 +0300

    Test two ready

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,8 +1,10 @@
 import datetime
 from freezegun import freeze_time
 
+
 @freeze_time("2012-01-01")
 def test_compose_datetime_from():
     from functions.level_1.two_date_parser import compose_datetime_from
 
-    assert compose_datetime_from("",time_str="18:12") == datetime.datetime(2012,1,1,18,12)
+    assert compose_datetime_from("", time_str="18:12") == datetime.datetime(2012, 1, 1, 18, 12)
+    assert compose_datetime_from("tomorrow", time_str="18:12") == datetime.datetime(2012, 1, 2, 18, 12)
