705df556a38a48a1d159155854aa0a06ead8630b
Denis Kondrashov <dkond@mail.ru>
2023-11-24 12:40:40 +0300

    Test five ready

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -2,4 +2,10 @@ from functions.level_1.five_title import change_copy_item
 
 
 def test_change_copy_item():
-    pass
+    assert change_copy_item("ЦБ может повысить", 5) == "ЦБ может повысить"
+    assert change_copy_item("ЦБ может повысить ключевую ставку", 5000) == "Copy of ЦБ может повысить ключевую ставку"
+    assert change_copy_item("Copy of ЦБ может повысить ключевую ставку", 5000) == "Copy of ЦБ может повысить ключевую ставку (2)"
+    assert change_copy_item("Copy of ЦБ может повысить ключевую ставку (2)", 5000) == "Copy of ЦБ может повысить ключевую ставку (3)"
+    assert change_copy_item("Copy of ЦБ может повысить ключевую ставку калябаля(2)", 5000) == "Copy of ЦБ может повысить ключевую ставку (3)"
+    assert change_copy_item("Copy of ЦБ может повысить(2) ключевую ставку (2)калябаля", 5000) == "Copy of ЦБ может повысить(2) ключевую ставку (3)" #WTF ?
+    assert change_copy_item("Copy of Copy of ЦБ может повысить ключевую ставку", 5000) == "Copy of Copy of ЦБ может повысить ключевую ставку (2)"
