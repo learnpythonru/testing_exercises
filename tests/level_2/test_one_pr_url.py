98e48dabd29ebcee568884c3c4e77b1a49309c72
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-27 19:34:03 +0300

    one done

--- tests/level_2/test_one_pr_url.py
+++ tests/level_2/test_one_pr_url.py
@@ -1,3 +1,30 @@
 import pytest
 
-from functions.level_2 import one_pr_url
+from functions.level_2.one_pr_url import is_github_pull_request_url
+
+
+def test__is_github_pull_request_url__succes():
+    correct_url = 'https://github.com/user/some_repository/pull/1'
+    result = is_github_pull_request_url(correct_url)
+    assert result == True
+
+
+@pytest.mark.parametrize('url, expected_result', [
+    ('github.com/user/some_repository/pull/1', False),
+    ('https://gitlub.com/user/some_repository/pull/1', False),
+    ('https://github.com/user/some_repository/push/1', False),
+])
+def test__is_github_pull_request_url__wrong_url(url, expected_result):
+    assert is_github_pull_request_url(url) == expected_result
+
+
+@pytest.mark.parametrize('url', [
+    ([
+       'https://github.com/user/some_repository/pull/1',
+       'https://github.com/user/some_repository/pull/2'
+    ]),
+    (100_000),
+])
+def test__is_github_pull_request_url__with_error(url):
+    with pytest.raises(AttributeError):
+        is_github_pull_request_url(url)
