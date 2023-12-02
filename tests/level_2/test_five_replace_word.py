b8a6eba29a72aca5f2ebd2fcb0482d57fa442a2c
Sergey Ilin <ilin_sergey90@mail.ru>
2023-12-02 16:43:46 +0300

    убрал закоментированный код и тесты на ошибки с типами данных

--- tests/level_2/test_five_replace_word.py
+++ tests/level_2/test_five_replace_word.py
@@ -3,19 +3,6 @@ import pytest
 from functions.level_2.five_replace_word import replace_word
 
 
-# def replace_word(text: str, replace_from: str, replace_to: str) -> str:
-#     words = text.split()
-
-#     new_words = []
-#     for word in words:
-#         if word.lower() == replace_from.lower():
-#             new_words.append(replace_to)
-#         else:
-#             new_words.append(word)
-
-#     return ' '.join(new_words)
-
-
 @pytest.mark.parametrize('text, replace_from, replace_to, expected_result', [
     ('На улице шел сильный снег!', 'сильный', 'слабый', 'На улице шел слабый снег!'),
     ('Это был замечательный день!', 'замечательный', 'замечательный', 'Это был замечательный день!'),
@@ -28,12 +15,3 @@ from functions.level_2.five_replace_word import replace_word
 def test__replace_word__succes(text, replace_from, replace_to, expected_result):
     assert replace_word(text=text, replace_from=replace_from,
                         replace_to=replace_to) == expected_result
-
-
-@pytest.mark.parametrize('text, replace_from, replace_to, expected_error', [
-    (123, '123', '321', AttributeError),
-    ('123 456 789', '123', 321, TypeError),
-])
-def test__replace_word__with_error(text, replace_from, replace_to, expected_error):
-    with pytest.raises(expected_error):
-        replace_word(text=text, replace_from=replace_from, replace_to=replace_to)
