cbc21e3904e4e0c71a59200fc6bbde6c5cfb485b
Octopulus96 <octopulus96@gmail.com>
2024-02-18 19:07:58 +0300

    Написал тесты для three_url_builder и two_date_parser

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,4 +2,8 @@ from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    pass
+    assert build_url("https://example.com", "api/data") == "https://example.com/api/data"
+    assert build_url("https://example.com", "api/data", {"param1": "value1", "param2": "value2"}) == "https://example.com/api/data?param1=value1&param2=value2"
+    assert build_url("https://example.com", "api/data", {}) == "https://example.com/api/data"
+    assert build_url("https://example.com", "api/data", {"param": "value"}) == "https://example.com/api/data?param=value"
+
--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -1,5 +1,7 @@
+import datetime
 from functions.level_1.two_date_parser import compose_datetime_from
 
 
 def test_compose_datetime_from():
-    pass
+    assert compose_datetime_from("today", "12:00") == datetime.datetime.now().replace(hour=12, minute=0, second=0, microsecond=0)
+    assert compose_datetime_from("tomorrow", "12:00") == (datetime.datetime.now() + datetime.timedelta(days=1)).replace(hour=12, minute=0, second=0, microsecond=0)
