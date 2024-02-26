49980313f98cdba63aa594f4157a0deb3e25953a
Jeffpython <69818613+Jeffpython@users.noreply.github.com>
2024-02-26 21:09:06 +0300

    Update three_first.py
    Результат выполнения print(type(first([], 'str'))):
    <class 'str'>

--- functions/level_2/three_first.py
+++ functions/level_2/three_first.py
@@ -1,7 +1,7 @@
 NOT_SET = "NOT_SET"
 
 
-def first(items: list[int], default: int | None | str = NOT_SET) -> int | None:
+def first(items: list[int], default: int | None | str = NOT_SET) -> int | None | str:
     if items:
         return items[0]
     if default == NOT_SET:
